4090012f41bf9b0c7e3c1ef556e39d603fb42104
VictorVatt <victor.vattier35@gmail.com>
2024-02-09 11:54:59 +0100

    add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,14 @@
 import Layout from '../components/Layout'; // Assurez-vous que le chemin d'accès est correct
 import '../styles/globals.css';
 import { DataProvider } from '@/context/context';
+import { Analytics } from '@vercel/analytics'
 
 function MyApp({ Component, pageProps }) {
     return (
         <DataProvider>
             <Layout>
                 <Component {...pageProps} />
+                <Analytics />
             </Layout>
         </DataProvider>
     );
