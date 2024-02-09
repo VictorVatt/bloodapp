3ed303c62e1183d552e5227e7671b244a689f79f
VictorVatt <victor.vattier35@gmail.com>
2024-02-09 13:49:00 +0100

    add gitignore

--- src/components/SavoirPlus/index.jsx
+++ src/components/SavoirPlus/index.jsx
@@ -27,13 +27,12 @@ const textes = {
     <br></br>
     <br></br> 
     Le ratio neutrophiles/lymphocytes, également connu sous le nom de NLR, est un indicateur significatif dans l'analyse sanguine des sportifs. Ce ratio est calculé en divisant le nombre de neutrophiles par le nombre de lymphocytes présents dans le sang. Les neutrophiles sont des cellules du système immunitaire impliquées dans la réponse inflammatoire, tandis que les lymphocytes jouent un rôle clé dans l'immunité spécifique. Un NLR élevé peut indiquer une inflammation systémique, souvent associée à un stress physique excessif ou à une surcharge d'entraînement. Surveiller l’évolution du NLR permet d’évaluer le niveau de récupération, de prévenir les risques de surentraînement et d’optimiser les performances. Une analyse régulière du NLR offre ainsi des informations précieuses sur l'état immunitaire et la capacité du corps à faire face aux contraintes de l'entraînement intensif, contribuant ainsi à une gestion plus éclairée de la santé des sportifs.
-    <br></br>
-    <br></br>
+    <div className={styles.modal_image_container}><Image src={"/images/inflammation.jpg"} width={500} height={150} alt='dezzdzd'/></div>
+
     <strong>Protéine C-réactive (CRP) et InterLeukines-6</strong>
     <br></br>
     <br></br>
     La protéine C-réactive (CRP) et les InterLeukines-6 (IL-6) constituent des marqueurs cruciaux lors de l'analyse sanguine des sportifs. La CRP et les IL-6 sont produits en réponse à une inflammation dans le corps, ce qui en fait un indicateur sensible des processus inflammatoires. Dans le contexte sportif, des niveaux élevés de CRP et IL-6 peuvent signaler une inflammation résultant de l'entraînement intensif ou d'une blessure. Surveiller ces marqueurs permet de détecter rapidement des signes d'inflammation excessive en fonction des valeurs seuils décrites.
-    <div className={styles.modal_image_container}><Image src={"/images/inflammation.jpg"} width={500} height={150} alt='dezzdzd'/></div>
     </p>,
     energy: <p>Ratio testostérone – cortisol
     <br></br>
